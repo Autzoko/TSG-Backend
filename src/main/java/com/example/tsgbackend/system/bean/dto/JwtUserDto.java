082097f13a729ea40dab1cf455f8e12f9555e755
Autzoko <llt020725@163.com>
2024-04-17 21:58:09 +0800

    introduce JwtUserDto

---
+++ src/main/java/com/example/tsgbackend/system/bean/dto/JwtUserDto.java
@@ -0,0 +1,60 @@
+package com.example.tsgbackend.system.bean.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class JwtUserDto implements UserDetails {
+
+    private final UserDto user;
+
+    @JsonIgnore
+    private final List<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends  GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return user.getEnabled();
+    }
+}
