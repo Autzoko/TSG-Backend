a1327387eb74172f89ad7c8cb6ef65713d852695
Autzoko <llt020725@163.com>
2024-04-20 22:16:31 +0800

    new LogAspect

---
+++ src/main/java/com/example/tsgbackend/logs/aspect/LogAspect.java
@@ -0,0 +1,12 @@
+package com.example.tsgbackend.logs.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+}
