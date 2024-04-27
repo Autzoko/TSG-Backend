52c971a4b746de6500addeca8fb39c06304b0fed
Autzoko <llt020725@163.com>
2024-04-27 10:20:55 +0800

    meta object handler

---
+++ src/main/java/com/example/tsgbackend/config/config/TsgMetaObjectHandler.java
@@ -0,0 +1,21 @@
+package com.example.tsgbackend.config.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TsgMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", () -> LocalDateTime.now(), LocalDateTime.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", () -> LocalDateTime.now(), LocalDateTime.class);
+    }
+}
