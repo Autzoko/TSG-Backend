1e91f1ad4602d92185f1fe5d5e2e06a48b82294a
Autzoko <llt020725@163.com>
2024-04-27 10:23:32 +0800

    config captcha constants

---
+++ src/main/java/com/example/tsgbackend/common/constant/CaptchaConstants.java
@@ -0,0 +1,23 @@
+package com.example.tsgbackend.common.constant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CaptchaConstants {
+
+    public static String codeType = "arithmetic";
+
+    public static Long expiration = 2L;
+
+    public static int length = 2;
+
+    public static int width = 111;
+
+    public static int height = 28;
+
+    public static String fontName;
+
+    public static int fontSize = 25;
+}
